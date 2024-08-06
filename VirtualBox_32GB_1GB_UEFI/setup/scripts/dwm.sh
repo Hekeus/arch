8eb01b29614949df1ee009d69f64b37ab29f4041
psplehanov <lapsusmemoriae664@gmail.com>
2024-08-06 14:09:17 +0500

    Update dwm.sh

--- VirtualBox_32GB_1GB_UEFI/setup/scripts/dwm.sh
+++ VirtualBox_32GB_1GB_UEFI/setup/scripts/dwm.sh
@@ -4,11 +4,26 @@
 set -x
 set -eo pipefail
 
+#сборка и установка виртуальной консоли st
+git clone https://aur.archlinux.org/st-git.git /home/user/setup/src/st
+cd /home/user/setup/src/st
+makepkg -si --noconfirm
+makepkg --clean
+
+#сборка и установка рабочего стола dwm
+git clone https://aur.archlinux.org/dwm-git.git /home/user/setup/src/dwm
+cd /home/user/setup/src/dwm
+makepkg -si --noconfirm
+makepkg --clean
+
+#установка меню для запуска приложений
+pacman -S --noconfirm dmenu
+
 
 #для пользователя user создать настройку Xorg
 touch /home/user/.xinitrc
 echo "exec dwm" >> /home/user/.xinitrc
-
+cd /home/user
 
 #----------------------------------
 exit
