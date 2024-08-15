963dc9d39c86cb5e86b1cf5306b1ec70d78e1e36
psplehanov <lapsusmemoriae664@gmail.com>
2024-08-15 11:39:13 +0500

    Update virtualbox.sh

--- VirtualBox_32GB_1GB_UEFI/setup/scripts/virtualbox.sh
+++ VirtualBox_32GB_1GB_UEFI/setup/scripts/virtualbox.sh
@@ -8,7 +8,7 @@ sudo modprobe -a vboxguest vboxsf vboxvideo
 sudo VBoxClient-all
 sudo mount -t vboxsf -o uid=1000,gid=1000 share /mnt
 
-sudo echo "share" /mnt vboxsf defaults,uid=1000,gid=1000,rw 0 0 >> /etc/fstab
+sudo sh -c 'echo "share /mnt vboxsf defaults,uid=1000,gid=1000,rw 0 0" >> /etc/fstab'
 
 
 #----------------------------------
