de488f60e9ea57550cda8cf5da06507ff2e0b30a
psplehanov <lapsusmemoriae664@gmail.com>
2024-08-15 11:40:03 +0500

    Update bootloader.sh

--- VirtualBox_32GB_1GB_UEFI/scripts/bootloader.sh
+++ VirtualBox_32GB_1GB_UEFI/scripts/bootloader.sh
@@ -9,7 +9,7 @@ set -eo pipefail
 
 UUIDroot=$(lsblk /dev/sda3 -dn -o UUID) 
 UUIDswap=$(lsblk /dev/sda2 -dn -o UUID)
-kernelparameters="root=UUID=$UUIDroot resume=UUID=$UUIDswap initrd=/initramfs-linux.img quiet ro"
+kernelparameters="root=UUID=$UUIDroot resume=UUID=$UUIDswap initrd=/initramfs-linux.img quiet rw"
 
 efibootmgr --create --disk /dev/sda --part 1 --label "arch" --loader /vmlinuz-linux --unicode "$kernelparameters"
 
