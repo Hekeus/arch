489783038c66cf1a32ccf1e484710192f683ad22
psplehanov <lapsusmemoriae664@gmail.com>
2024-08-19 14:03:13 +0500

    Update environment.sh

--- VirtualBox_32GB_1GB_UEFI/setup/scripts/environment.sh
+++ VirtualBox_32GB_1GB_UEFI/setup/scripts/environment.sh
@@ -5,10 +5,10 @@ set -eo pipefail
 
 sudo touch /etc/profile.d/env.sh
 sudo sh -c 'echo "export EDITOR=vim" >> /etc/profile.d/env.sh'
-sudo sh -c 'echo "export XDG_CACHE_HOME=$HOME/.cache" >> /etc/profile.d/env.sh'
-sudo sh -c 'echo "export XDG_CONFIG_HOME=$HOME/.config" >> /etc/profile.d/env.sh'
-sudo sh -c 'echo "export XDG_DATA_HOME=$HOME/.local/share" >> /etc/profile.d/env.sh'
-sudo sh -c 'echo "export XDG_STATE_HOME=$HOME/.local/state" >> /etc/profile.d/env.sh'
+sudo sh -c 'echo "export XDG_CACHE_HOME=/home/user/.cache" >> /etc/profile.d/env.sh'
+sudo sh -c 'echo "export XDG_CONFIG_HOME=/home/user/.config" >> /etc/profile.d/env.sh'
+sudo sh -c 'echo "export XDG_DATA_HOME=/home/user/.local/share" >> /etc/profile.d/env.sh'
+sudo sh -c 'echo "export XDG_STATE_HOME=/home/user/.local/state" >> /etc/profile.d/env.sh'
 
 #----------------------------------
 exit
