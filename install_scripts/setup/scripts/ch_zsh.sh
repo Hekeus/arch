84d7f82a245dc1f17ce873bf1b631243e9a92c1e
psplehanov <lapsusmemoriae664@gmail.com>
2024-08-26 15:57:09 +0500

    Update ch_zsh.sh

--- install_scripts/setup/scripts/ch_zsh.sh
+++ install_scripts/setup/scripts/ch_zsh.sh
@@ -11,4 +11,4 @@ mkdir -p /home/user/.config/zsh
 mkdir -p /home/user/.cache/zsh
 chown -cR user:user /home/user/.config
 chown -cR user:user /home/user/.cache
-cp /home/user/setup/package/.zshrc /home/user/.config/zsh/.zshrc
+cp /home/user/setup/packages/.zshrc /home/user/.config/zsh/.zshrc
