fae25ebb99651fbfcb0db622d8e0a38959ed9e48
psplehanov <lapsusmemoriae664@gmail.com>
2024-08-26 12:31:17 +0500

    Update ch_users.sh

--- install_scripts/setup/scripts/ch_users.sh
+++ install_scripts/setup/scripts/ch_users.sh
@@ -29,12 +29,13 @@ sh -c 'echo "export XDG_CACHE_HOME=/home/user/.cache" >> /etc/profile.d/env.sh'
 sh -c 'echo "export XDG_CONFIG_HOME=/home/user/.config" >> /etc/profile.d/env.sh'
 sh -c 'echo "export XDG_DATA_HOME=/home/user/.local/share" >> /etc/profile.d/env.sh'
 sh -c 'echo "export XDG_STATE_HOME=/home/user/.local/state" >> /etc/profile.d/env.sh'
-sh -c 'echo "export ZDOTDIR=/home/user/.config/zsh" >> /etc/profile.d/env.sh'
-sh -c 'echo "export ZSH_CACHE_DIR=/home/user/.cache/zsh" >> /etc/profile.d/env.sh'
-
 
 
 #настройка zsh
+touch /etc/zsh/zshenv
+sh -c 'echo "export ZDOTDIR=/home/user/.config/zsh" >> /etc/zsh/zshenv'
+sh -c 'echo "export ZSH_CACHE_DIR=/home/user/.cache/zsh" >> /etc/zsh/zshenv'
+
 mkdir -p /home/user/.config/zsh
 mkdir -p /home/user/.cache/zsh
 chown -cR user:user /home/user/.config
