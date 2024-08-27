7976bf536debfcb71d3039eb348e094b661266b7
psplehanov <lapsusmemoriae664@gmail.com>
2024-08-27 12:27:20 +0500

    1

--- install_scripts/setup/scripts/ch_zsh.sh
+++ install_scripts/setup/scripts/ch_zsh.sh
@@ -9,6 +9,6 @@ sh -c 'echo "export ZSH_CACHE_DIR=/home/user/.cache/zsh" >> /etc/zsh/zshenv'
 
 mkdir -p /home/user/.config/zsh
 mkdir -p /home/user/.cache/zsh
+cp /home/user/setup/packages/.zshrc /home/user/.config/zsh/.zshrc
 chown -cR user:user /home/user/.config
 chown -cR user:user /home/user/.cache
-cp /home/user/setup/packages/.zshrc /home/user/.config/zsh/.zshrc
