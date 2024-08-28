7f2529cb91cffaa70caf74b5f40f74d8a4b89af0
psplehanov <lapsusmemoriae664@gmail.com>
2024-08-28 16:39:47 +0500

    rehash zsh

--- install_scripts/setup/scripts/pacman.sh
+++ install_scripts/setup/scripts/pacman.sh
@@ -9,3 +9,17 @@ doas sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 15/' /etc/pacman.conf
 doas sed -i 's/#NoUpgrade   =/NoUpgrade   = usr/bin/sh/' /etc/pacman.conf
 doas sed -i 's/#NoExtract   =/NoExtract   = usr/bin/sh/' /etc/pacman.conf
 
+doas mkdir -p /etc/pacman.d/hooks
+doas touch /etc/pacman.d/hooks/zsh-rehash.hook
+doas sh -c 'echo "[Trigger]" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "Operation = Install" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "Operation = Upgrade" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "Operation = Remove" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "Type = Path" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "Target = usr/bin/*" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "\n" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "[Action]" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "Depends = zsh" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "Depends = procps-ng" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "When = PostTransaction" >> /etc/pacman.d/hooks/zsh-rehash.hook'
+doas sh -c 'echo "Exec = /usr/bin/pkill zsh --signal=USR1" >> /etc/pacman.d/hooks/zsh-rehash.hook'
--- packages/.zshrc
+++ packages/.zshrc
@@ -101,21 +101,7 @@ export LISTMAX=256
 #------------------------------------------
 #Выполнение rehash по необходимости
 
-zshcache_time="$(date +%s%N)"
-
-autoload -Uz add-zsh-hook
-
-rehash_precmd() {
-  if [[ -a /var/cache/zsh/pacman ]]; then
-    local paccache_time="$(date -r /var/cache/zsh/pacman +%s%N)"
-    if (( zshcache_time < paccache_time )); then
-      rehash
-      zshcache_time="$paccache_time"
-    fi
-  fi
-}
-
-add-zsh-hook -Uz precmd rehash_precmd
+TRAPUSR1() { rehash }
 #------------------------------------------
 
 #------------------------------------------
