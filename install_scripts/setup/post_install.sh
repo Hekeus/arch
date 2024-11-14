b8083e225edb9df073ff66bf669d9353644ff37a
psplehanov <psplehanov@outlook.com>
2024-11-14 16:43:06 +0500

    install zathura telegram obsidian

--- install_scripts/setup/post_install.sh
+++ install_scripts/setup/post_install.sh
@@ -14,6 +14,7 @@ sh $BASEDIR/scripts/makepkg.sh
 sh $BASEDIR/scripts/dwm.sh
 #sh $BASEDIR/scripts/sound.sh
 sh $BASEDIR/scripts/yandex_disk.sh
+sh $BASEDIR/scripts/software.sh
 sh $BASEDIR/scripts/clean.sh
 
 shutdown -r now
---
+++ install_scripts/setup/scripts/software.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -x
+doas pacman -S --noconfirm telegram-desktop zathura obsidian
+
+exit
