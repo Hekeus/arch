b433970f6ab8ba8583377339e68ca688866c6e3c
psplehanov <psplehanov@outlook.com>
2024-11-29 15:30:25 +0500

    change sel color dwm

--- themes/all_i_need/PKGBUILDS/dwm/config.h
+++ themes/all_i_need/PKGBUILDS/dwm/config.h
@@ -12,10 +12,11 @@ static const char col_gray2[]       = "#1E3657";
 static const char col_gray3[]       = "#BDBCB5";
 static const char col_gray4[]       = "#FCFBED";
 static const char col_cyan[]        = "#1E3657";
+static const char col_sel[]         = "#FEEACF";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1,  col_gray3  },
+	[SchemeSel]  = { col_sel, col_gray1,  col_sel  },
 };
 
 /* tagging */
